a2c08690d6f973803d5be44e11993c4e0faa6983
Francesco Bailo <me@francescobailo.net>
2016-06-05 12:06:44 +0200

    Removed unnecessary files and dirs

--- phpsite/footer.php
+++
@@ -1,7 +0,0 @@
-
-
-<div = 'footer'> Data sources: <a href = 'http://cercauniversita.cineca.it/php5/docenti/cerca.php'>cineca.it</a>; <a href = 'http://www.paginebianche.it/contacognome'>paginebinache.it</a>; <a href = 'http://dati.istat.it/'>dati.istat.it</a>. Francesco Bailo 2016 (Draft version)</div>
-
-   </div>
-</body>
-</html>
